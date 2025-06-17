20486f2c8918601d75113b735b8a2b2a0baa1cec
Fernando Henrique <fernandohenriqsilva@gmail.com>
2025-06-17 20:06:09 -0300

    refactor: move AppModule to a separate module file for better organization
    - Extracted the AppModule class from index.ts to a new module.ts file.
    - Updated index.ts to import AppModule and initialize it with the main configuration.

--- src/index.ts
+++ src/index.ts
@@ -1,79 +1,9 @@
-import express from 'express';
 import 'reflect-metadata';
-import { container } from 'tsyringe';
 
 import { UserController } from './exemple/user.controller';
 import { Db, UserRepoMemory } from './exemple/user.repo.memory';
 import { UserService } from './exemple/user.service';
-
-container.registerSingleton('UserService', UserService);
-container.registerSingleton('UserRepo', UserRepoMemory);
-
-export type Provider = { key: string; provide: any } | (new (...args: any[]) => any);
-export type Controller = new (...args: any[]) => { [key: string]: any };
-
-export class AppModule {
-  private app: express.Application = express();
-  private initialized: boolean = false;
-  private bootstrapped: boolean = false;
-
-  constructor(
-    readonly options: {
-      path?: string;
-      providers: Provider[];
-      controllers: Controller[];
-    },
-  ) {
-    this.app.use(express.json());
-  }
-
-  async bootstrap() {
-    if (this.bootstrapped) return this;
-
-    const { path: modulePath = '/', providers, controllers } = this.options;
-
-    // register providers
-    for (const provider of providers) {
-      const key = provider instanceof Function ? provider.name : provider.key;
-      const provide = provider instanceof Function ? provider : provider.provide;
-      container.registerSingleton(key, provide);
-    }
-
-    // resolve controllers
-    for (const controller of controllers) {
-      const controllerInstance = container.resolve(controller);
-      const routes: {
-        method: 'get' | 'post' | 'put' | 'patch' | 'delete';
-        path: string;
-        handler: (ctx: { req: any; res: any; next: Function }) => Promise<any>;
-        propertyKey: string;
-      }[] = Reflect.getMetadata('controller:routes', controller) || [];
-      for (const route of routes) {
-        const fullPath = `${modulePath}${route.path}`.replace('//', '/');
-        this.app[route.method](fullPath, (req: any, res: any, next: Function) => {
-          const ctx = { req, res, next };
-          try {
-            return controllerInstance[route.propertyKey](ctx);
-          } catch (error) {
-            next(error);
-          }
-        });
-      }
-    }
-    this.bootstrapped = true;
-    return this;
-  }
-
-  async listen(port: number) {
-    if (this.initialized) return this;
-    if (!this.bootstrapped) await this.bootstrap();
-    this.app.listen(port, () => {
-      console.info(`Server is running on port ${port}`);
-    });
-    this.initialized = true;
-    return this;
-  }
-}
+import { AppModule } from './lib/module';
 
 const mainModule = new AppModule({
   path: '/',

3c6e3a9f0e7fd6aee1edb68f074866f6bc243309
Fernando Henrique <fernandohenriqsilva@gmail.com>
2025-06-19 04:40:15 -0300

    refactor: enhance main and user modules with improved structure and dependency injection
    - Updated main module to integrate UserModule and register providers for UserService and MemoryDb.
    - Refactored user module to extend AppModule, simplifying its structure and enhancing controller registration.
    - Improved dependency injection for user-related services, promoting better modularization and maintainability.

--- src/exemple/modules/main.module.ts
+++ src/exemple/modules/main.module.ts
@@ -1,8 +1,22 @@
 import { AppModule } from '../../lib/module';
 import { companyModule } from './company/company.module';
-import { userModule } from './user/user.module';
+import { UserModule } from './user/user.module';
+import { IMemoryDb, UserRepoMemory } from './user/user.repo.memory';
+import { UserService } from './user/user.service';
+
+class MemoryDb implements IMemoryDb {
+  users: any[] = [];
+}
 
 export const mainModule = new AppModule({
   path: '/api',
-  imports: [userModule, companyModule],
+  providers: [
+    { key: 'UserService', provide: UserService },
+    { key: 'UserRepo', provide: UserRepoMemory },
+    {
+      key: 'MemoryDb',
+      provide: MemoryDb,
+    },
+  ],
+  imports: [UserModule, companyModule],
 });
--- src/exemple/modules/user/user.module.ts
+++ src/exemple/modules/user/user.module.ts
@@ -3,19 +3,11 @@ import { UserController } from './user.controller';
 import { IMemoryDb, UserRepoMemory } from './user.repo.memory';
 import { UserService } from './user.service';
 
-class MemoryDb implements IMemoryDb {
-  users: any[] = [];
+export class UserModule extends AppModule {
+  constructor() {
+    super({
+      path: '/users',
+      controllers: [UserController],
+    });
+  }
 }
-
-export const userModule = new AppModule({
-  path: '/users',
-  providers: [
-    { key: 'UserService', provide: UserService },
-    { key: 'UserRepo', provide: UserRepoMemory },
-    {
-      key: 'MemoryDb',
-      provide: MemoryDb,
-    },
-  ],
-  controllers: [UserController],
-});
